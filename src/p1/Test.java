c93e6bda2f8c6ba04f556f53ddfa152f76f50689
gaojun <gaojun168@163.com>
2020-07-18 12:21:54 +0800

    1

---
+++ src/p1/Test.java
@@ -0,0 +1,11 @@
+package p1;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// 汉字测试
+		System.out.println("汉字测试   汉字测试   汉字测试");
+        //https://github.com/gaojun168/Z82.git
+	}
+
+}
